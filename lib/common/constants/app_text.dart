fbc7c45240ff149e55c690c2759319ba63342f3c
Davi Monteiro <montsgit@gmail.com>
2025-02-19 15:58:12 -0300

    Classes for Decoration

---
+++ lib/common/constants/app_text.dart
@@ -0,0 +1,3 @@
+class Texts {
+  Texts._();
+}
---
+++ lib/common/constants/app_textfield.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class TextFields {
+  static final primaryInput = InputDecoration(
+    border: OutlineInputBorder(),
+  );
+}
