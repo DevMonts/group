dc32b8d7f5e0c176ed0f0d64c1eb64084c3bd2c3
Davi Monteiro <montsgit@gmail.com>
2025-02-19 18:02:57 -0300

    Update register_page.dart

--- lib/features/aut/register_page.dart
+++ lib/features/aut/register_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:group/features/main/main_page.dart';
 
+import '../../common/constants/app_textfield.dart';
+
 class Register extends StatefulWidget {
   const Register({super.key});
 
@@ -9,6 +11,9 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,11 +25,79 @@ class _RegisterState extends State<Register> {
           TextField(
             decoration: InputDecoration(labelText: 'Email'),
           ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Senha'),
+          TextFormField(
+            decoration: TextFields.primaryInput.copyWith(
+              labelText: 'Senha',
+              hintText: 'Digite sua senha',
+              suffixIcon: _obscureText
+                  ? IconButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            _obscureText = !_obscureText;
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.visibility_off),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            _obscureText = !_obscureText;
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.visibility),
+                    ),
+            ),
+            obscureText: _obscureText,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Digite sua senha';
+              }
+              if (value.length < 6) {
+                return 'A senha deve ter no mínimo 6 dígitos';
+              }
+              return null;
+            },
           ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Confirmar Senha'),
+          TextFormField(
+            decoration: TextFields.primaryInput.copyWith(
+              labelText: 'Confirmar Senha',
+              hintText: 'Confirme sua senha',
+              suffixIcon: _obscureText
+                  ? IconButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            _obscureText = !_obscureText;
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.visibility_off),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(
+                          () {
+                            _obscureText = !_obscureText;
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.visibility),
+                    ),
+            ),
+            obscureText: _obscureText,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Digite uma senha';
+              }
+              if (value.length < 6) {
+                return 'A senha deve ter no mínimo 6 dígitos';
+              }
+              return null;
+            },
           ),
           ElevatedButton(
             onPressed: () {
